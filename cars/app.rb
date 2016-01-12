1b63521197a8cef9c14f174440b3b0577a1f6595
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-01-12 10:02:29 -0500

    Add Car example

---
+++ cars/app.rb
@@ -0,0 +1,7 @@
+# app.rb
+
+require_relative("lib/car.rb")
+
+# variable and methods names should be lowercase_with_underscores
+my_car = Car.new
+my_car.rev
---
+++ cars/lib/car.rb
@@ -0,0 +1,8 @@
+# lib/car.rb
+
+# class names should be CapitalizedLikeThis
+class Car
+  def rev
+    puts "Vroooooom"
+  end
+end
